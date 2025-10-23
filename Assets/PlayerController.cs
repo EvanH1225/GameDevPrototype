b13ff4e572d996ad720209ac9ec8cc8ab604bb1b
atjenks <124316069+atjenks@users.noreply.github.com>
2025-10-23 14:28:51 +0100

    v1 player

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    // Movement settings
+    public float moveSpeed = 5f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 2f;
+
+    // Mouse settings
+    public float mouseSensitivity = 1f;
+    public Transform playerCamera;
+
+    private CharacterController controller;
+    private Vector3 velocity;
+    private float xRotation = 0f;
+    private Vector2 moveInput;
+    private Vector2 lookInput;
+    private bool jumpPressed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovement();
+        HandleLook();
+
+        Debug.DrawRay(transform.position, Vector3.down * 1.1f, Color.green);
+        Debug.Log("Grounded: " + controller.isGrounded);
+    }
+
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>();
+    }
+
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            jumpPressed = true;
+        }
+
+        if (context.canceled)
+        {
+            jumpPressed = false;
+        }
+        Debug.Log("Jump Action: " + context.phase);
+    }
+
+
+    void HandleMovement()
+    {
+        Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
+        move = move * moveSpeed;
+
+        if (controller.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        Debug.Log("Grounded: " + controller.isGrounded);
+        if (jumpPressed && controller.isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move((move + velocity) * Time.deltaTime);
+    }
+
+
+    void HandleLook()
+    {
+        float mouseX = lookInput.x * mouseSensitivity * Time.deltaTime * 100f;
+        float mouseY = lookInput.y * mouseSensitivity * Time.deltaTime * 100f;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+}
