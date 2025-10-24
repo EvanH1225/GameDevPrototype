38387693821046875d69ee7c6951fbbc078e14eb
Evan <evan.henkle@gmail.com>
2025-10-24 01:37:46 +0100

    enemies can be hit and die as well as take some knockback, also only spawn at night

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -21,10 +22,14 @@ public class PlayerController : MonoBehaviour
     private Vector2 lookInput;
     private bool jumpPressed;
 
+    public float playerRange = 6f;
+
     private bool pickupPressed;
-    public float pickupRange;
     public LayerMask pickupLayer;
     public InventoryManager inventoryManager;
+
+    private bool attackPressed;
+    public LayerMask attackLayer;
 
 
     // Start is called before the first frame update
@@ -43,6 +48,7 @@ public class PlayerController : MonoBehaviour
         HandleMovement();
         HandleLook();
         HandlePickup();
+        HandleAttack();
     }
 
 
@@ -76,7 +82,25 @@ public class PlayerController : MonoBehaviour
         if (context.performed)
         {
             pickupPressed = true;
-        } 
+        }
+
+        if (context.canceled)
+        {
+            pickupPressed = false;
+        }
+    }
+    
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            attackPressed = true;
+        }
+
+        if (context.canceled)
+        {
+            attackPressed = false;
+        }
     }
 
 
@@ -118,20 +142,42 @@ public class PlayerController : MonoBehaviour
         if (pickupPressed)
         {
             Ray ray = new Ray(playerCamera.position, playerCamera.forward);
-            Debug.DrawRay(ray.origin, ray.direction * pickupRange, Color.green, 2f);
 
-            if (Physics.Raycast(ray, out RaycastHit hit, pickupRange, pickupLayer))
+            if (Physics.Raycast(ray, out RaycastHit hit, playerRange, pickupLayer))
             {
-                Debug.Log("WELL WE HIT SOMETHING");
                 Item item = hit.collider.GetComponent<Item>();
                 if (item != null)
                 {
-                    Debug.Log("HIT AN ITEM!!!");
                     item.OnPickup(inventoryManager);
                 }
             }
-            
+
             pickupPressed = false;
+        }
+    }
+    
+    void HandleAttack()
+    {
+        if (attackPressed)
+        {
+            Ray ray = new Ray(playerCamera.position, playerCamera.forward);
+
+            if (Physics.Raycast(ray, out RaycastHit hit, playerRange, attackLayer))
+            {
+                Enemy enemy = hit.collider.GetComponent<Enemy>();
+                if (enemy == null)
+                {
+                    enemy = hit.collider.GetComponentInParent<Enemy>();
+                }
+                if (enemy != null)
+                {
+                    Debug.Log("HIT ENEMY");
+                    enemy.TakeDamage();
+                    enemy.ApplyKnockback(playerCamera.position);
+                }
+            }
+
+            attackPressed = false;
         }
     }
 }
